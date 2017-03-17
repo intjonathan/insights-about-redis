796510c9e663974e5d843b2a91336a620b587d39
Jonathan Owens <jonathan@newrelic.com>
2017-03-17 16:23:12 -0700

    Working version

--- redis-insights.rb
+++ redis-insights.rb
@@ -1,6 +1,10 @@
+#!/usr/bin/env ruby
+
+require 'resolv'
 require 'clockwork'
 require 'redis'
 require 'httparty'
+require 'oj'
 
 REDIS_HOST = ENV['REDIS_HOST'] || 'localhost'
 REDIS_PORT = ENV['REDIS_PORT'] || 6379
@@ -11,14 +15,23 @@ INSIGHTS_EVENT_TYPE   = ENV['INSIGHTS_EVENT_TYPE'] || 'RedisInfo'
 module Clockwork
   
   @redis = Redis.new(:host => REDIS_HOST, :port => REDIS_PORT)
+  @redis_host_ip = Resolv.getaddress(REDIS_HOST)
 
-  every 30.seconds, 'pull redis INFO and post to Insights' do
+  every 20.seconds, 'pull redis INFO and post to Insights' do
     info = @redis.info
-    info['eventType'] = INSIGHTS_EVENT_TYPE
-    info['eventVersion'] = 1
+    info.merge!('host_name' => REDIS_HOST,
+                'host_ip'  => @redis_host_ip,
+                'eventType' => INSIGHTS_EVENT_TYPE,
+                'eventVersion' => 1)
+    info = Hash[ info.map { | k,v| [k, to_number(v)] } ]
     response = HTTParty.post(INSIGHTS_EVENT_URL,
-            :body    => Oj.dump(events),
+            :body    => Oj.dump(info),
             :headers => {'Content-Type' => 'application/json',
                          'X-Insert-Key' => INSIGHTS_INSERT_KEY})    
+    puts response
   end
+end
+
+def to_number(string)
+  Integer(string) rescue Float(string) rescue string
 end
