d5b47b8196690ce0e07ccb37c272e8307d317d4a
Jonathan Owens <jonathan@newrelic.com>
2017-03-18 15:23:24 -0700

    Convert to REDIS_URL to support extended connection information.

--- redis-insights.rb
+++ redis-insights.rb
@@ -1,17 +1,35 @@
 #!/usr/bin/env ruby
 
+require 'uri'
 require 'resolv'
+
 require 'clockwork'
 require 'redis'
 require 'httparty'
 require 'oj'
 
 REPORT_FREQUENCY_SEC = ENV['REPORT_FREQUENCY_SEC'] || 20
-REDIS_HOST           = ENV['REDIS_HOST'] || 'localhost'
-REDIS_PORT           = ENV['REDIS_PORT'] || 6379
+REDIS_URL            = ENV['REDIS_URL'] || 'redis://localhost:6379'
 INSIGHTS_INSERT_KEY  = ENV['INSIGHTS_INSERT_KEY']
 INSIGHTS_EVENT_URL   = ENV['INSIGHTS_EVENT_URL']
 INSIGHTS_EVENT_TYPE  = ENV['INSIGHTS_EVENT_TYPE'] || 'RedisInfo'
+
+def to_number(string)
+  Integer(string) rescue Float(string) rescue string
+end
+
+def redis_host_info(redis_url)
+  redis_uri = URI(redis_url)
+
+  @host_ip =
+    if redis_uri.host == 'localhost'
+      Resolv.getaddress(Socket.gethostname)
+    else
+      Resolv.getaddress(redis_uri.host)
+    end
+
+  [@host_ip, redis_uri.host]
+end
 
 module Clockwork
   begin
@@ -22,12 +40,12 @@ module Clockwork
     exit 1
   end
 
-  @redis = Redis.new(:host => REDIS_HOST, :port => REDIS_PORT)
-  @redis_host_ip = Resolv.getaddress(REDIS_HOST)
+  @redis = Redis.new(:url => REDIS_URL)
+  @redis_host_ip, @redis_host_name = redis_host_info(REDIS_URL)
 
   every @report_frequency.seconds, 'pull redis INFO and post to Insights' do
     info = @redis.info
-    info.merge!('host_name' => REDIS_HOST,
+    info.merge!('host_name' => @redis_host_name,
                 'host_ip'  => @redis_host_ip,
                 'eventType' => INSIGHTS_EVENT_TYPE,
                 'eventVersion' => 1)
@@ -35,11 +53,9 @@ module Clockwork
     response = HTTParty.post(INSIGHTS_EVENT_URL,
             :body    => Oj.dump(info),
             :headers => {'Content-Type' => 'application/json',
-                         'X-Insert-Key' => INSIGHTS_INSERT_KEY})    
+                         'X-Insert-Key' => INSIGHTS_INSERT_KEY})
     puts response
   end
+
 end
 
-def to_number(string)
-  Integer(string) rescue Float(string) rescue string
-end
